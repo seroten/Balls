21443015c38344dedc5c797f0e746a16c762e1a6
Dmitry <seroten@mail.ru>
2020-07-09 09:51:33 +0300

    create project

---
+++ src/MainCircles.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class MainCircles extends JFrame{
+    private static final int POS_X = 200;
+    private static final int POS_Y = 100;
+    private static final int WINDOW_WIDTH = 800;
+    private static final int WINDOW_HEIGHT = 600;
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainCircles();
+            }
+        });
+    }
+
+    private MainCircles() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(POS_X, POS_Y, WINDOW_WIDTH, WINDOW_HEIGHT);
+        setTitle("Circles");
+        setVisible(true);
+    }
+}
