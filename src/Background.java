8d631a6ef95f57133728ccc0136556e3b675a7de
Dmitry <seroten@mail.ru>
2020-07-10 12:02:55 +0300

    little changes

--- src/Background.java
+++ src/Background.java
@@ -18,9 +18,9 @@ public class Background{
         rgb[0] += incrementR;
         rgb[1] += incrementG;
         rgb[2] += incrementB;
-        if(rgb[0] > 255 || rgb[0] < 0) {rgb[0] = rgb[0] - incrementR; incrementR = incrementR*(-1);}
-        if(rgb[1] > 255 || rgb[1] < 0) {rgb[1] = rgb[1] - incrementG; incrementG = incrementG*(-1);}
-        if(rgb[2] > 255 || rgb[2] < 0) {rgb[2] = rgb[2] - incrementB; incrementB = incrementB*(-1);}
+        if(rgb[0] > 255 || rgb[0] < 0) {rgb[0] -= incrementR; incrementR *= (-1);}
+        if(rgb[1] > 255 || rgb[1] < 0) {rgb[1] -= incrementG; incrementG *= (-1);}
+        if(rgb[2] > 255 || rgb[2] < 0) {rgb[2] -= incrementB; incrementB *= (-1);}
         return rgb;
     }
     public Color render() {
